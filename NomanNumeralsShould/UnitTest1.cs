15bd629c43f50f0ac51d63313e687271a85e7119
David Barcón <dbarcon@domingoalonsogroup.com>
2023-12-05 10:04:43 +0000

    Agregar archivos de proyecto.

---
+++ NomanNumeralsShould/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NomanNumeralsShould
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
